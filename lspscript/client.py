c7a71cf7633ca3c3b8482c6231055ef157f2707d
Jonas Rinke <delphi1024@gmail.com>
2022-11-13 22:09:18 +0100

    Added launch_command param to ServerLaunchParams

--- lspscript/client.py
+++ lspscript/client.py
@@ -8,19 +8,26 @@ from threading import Thread
 from typing import Any, Callable, Optional, Sequence, Tuple, Union
 
 from lspscript.generated.dispatch import (write_notification_params,
-                                          write_request_params)
+                                          write_request_params,
+                                          parse_request_result)
 from lspscript.generated.util import JSON_VALUE
 from lspscript.protocol import (LSPException, LSProtocol, LSStreamingProtocol,
                                 LSSubprocessProtocol)
 
 
 class _ServerLaunchParams:
-    server_path: Path
+    server_path: Optional[Path]
+    launch_command: Optional[str]
     additional_args: Sequence[str]
     additional_only: bool
 
-    def __init__(self, *, server_path: Path, additional_args: Sequence[str] = [], additional_only: bool = False) -> None:
+    def __init__(self, *,
+                 server_path: Optional[Path] = None,
+                 launch_command: Optional[str] = None,
+                 additional_args: Sequence[str] = [],
+                 additional_only: bool = False) -> None:
         self.server_path = server_path
+        self.launch_command = launch_command
         self.additional_args = additional_args
         self.additional_only = additional_only
 
@@ -31,16 +38,28 @@ class _ServerLaunchParams:
 class StdIOConnectionParams(_ServerLaunchParams):
     """Launch parameters which launch a language server with stdio communication."""
 
-    def __init__(self, *, server_path: Path, additional_args: Sequence[str] = [], additional_only: bool = False) -> None:
+    def __init__(self, *,
+                 server_path: Optional[Path] = None,
+                 launch_command: Optional[str] = None,
+                 additional_args: Sequence[str] = [],
+                 additional_only: bool = False) -> None:
         """Constructs a new StdIOConnectionParams instance. This instance can then be
         used to launch a new client/server connection.
 
+        Either `server_path` or `launch_command` must be set.
+
         Parameters:
         - `server_path`: The path were the language server executable is located.
+        - `launch_command`: Shell command to start the server. If a launch command is given,
+          no additional arguments are appended. This means that the caller may also need
+          to add `--stdio` to the command to select a connection via standard input/output streams.
         - `additional_args`: List of additional arguments to pass to the server.
         - `additional_only`: Do not send any standard arguments, only those in `additional_args`."""
 
-        super().__init__(server_path=server_path, additional_args=additional_args, additional_only=additional_only)
+        if not server_path and not launch_command:
+            raise ValueError("Either server_path or launch_command need to be set.")
+
+        super().__init__(server_path=server_path, additional_args=additional_args, launch_command=launch_command, additional_only=additional_only)
 
     async def launch_server_from_event_loop(self, receive_callback: Callable[[str, JSON_VALUE], None]) -> Tuple[BaseTransport, LSProtocol]:
         args = ["--stdio", f"--clientProcessId={getpid()}"]
@@ -49,18 +68,23 @@ class StdIOConnectionParams(_ServerLaunchParams):
         else:
             args += self.additional_args
         loop = get_running_loop()
-        return await loop.subprocess_exec(lambda: LSSubprocessProtocol(receive_callback), self.server_path, *args)
+        if self.server_path:
+            return await loop.subprocess_exec(lambda: LSSubprocessProtocol(receive_callback), self.server_path, *args)
+        elif self.launch_command:
+            return await loop.subprocess_shell(lambda: LSSubprocessProtocol(receive_callback), self.launch_command)
+        else:
+            assert False
 
 
 class SocketConnectionParams(_ServerLaunchParams):
     """Launch parameters which launch a language server with tcp socket communication."""
 
-    server_path_opt: Optional[Path]
     hostname: str
     port: int
 
     def __init__(self, *,
-                server_path: Optional[Path],
+                server_path: Optional[Path] = None,
+                launch_command: Optional[str] = None,
                 port: int,
                 hostname: str = "localhost",
                 additional_args: Sequence[str] = [],
@@ -68,19 +92,22 @@ class SocketConnectionParams(_ServerLaunchParams):
         """Constructs a new SocketConnectionParams instance. This instance can then
         be used to launch a new client/server connection.
 
+        If neither `server_path` nor `launch_command` are set, no server is launched. Instead,
+        the Client tries to connect to an already running server.
+
         Parameters:
-        - `server_path`: The path were the language server executable is located. When connecting
-           to an already running server, the server_path may be `None`.
+        - `server_path`: The path were the language server executable is located.
+        - `launch_command`: Shell command to start the server. If a launch command is given,
+          no additional arguments are appended. This means that the caller may also need
+          to add `--socket=<port>` to the command to select a connection via TCP-sockets.
         - `port`: The port number to use for the TCP connection.
         - `hostname`: An optional name of a host to connect to. The default is 'localhost'.
         - `additional_args`: List of additional arguments to pass to the server.
         - `additional_only`: Do not send any standard arguments, only those in `additional_args`."""
 
-        self.server_path_opt = server_path
+        super().__init__(server_path=server_path, launch_command=launch_command, additional_args=additional_args, additional_only=additional_only)
         self.hostname = hostname
         self.port = port
-        self.additional_args = additional_args
-        self.additional_only = additional_only
 
     async def launch_server_from_event_loop(self, receive_callback: Callable[[str, JSON_VALUE], None]) -> Tuple[BaseTransport, LSProtocol]:
         args = [f"--socket={self.port}", f"--clientProcessId={getpid()}"]
@@ -89,8 +116,10 @@ class SocketConnectionParams(_ServerLaunchParams):
         else:
             args += self.additional_args
 
-        if self.server_path_opt:
-            subprocess.Popen([self.server_path_opt.absolute()] + list(args))
+        if self.server_path:
+            subprocess.Popen([self.server_path.absolute()] + list(args))
+        elif self.launch_command:
+            subprocess.Popen(self.launch_command, shell=True)
 
         loop = get_running_loop()
         return await loop.create_connection(lambda: LSStreamingProtocol(receive_callback), host=self.hostname, port=self.port, family=AF_INET)
@@ -145,7 +174,7 @@ class Client:
         await future
 
         if result := future.result():
-            return result
+            return parse_request_result[method](result)
         elif exception := future.exception():
             return exception
         else:

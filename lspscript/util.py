bb108ae652b7b332761044725018522f64a17881
Jonas Rinke <delphi1024@gmail.com>
2023-03-12 20:50:52 +0100

    Added languages module

--- lspscript/util.py
+++ lspscript/util.py
@@ -5,262 +5,10 @@ from pathlib import Path, WindowsPath
 from typing import Generator, List, Optional
 from urllib.parse import urlsplit
 
+from lspscript.languages import extension_to_language_id
 from lspscript.types.enumerations import FileOperationPatternKind
 
 from .types.structures import FileOperationFilter, TextDocumentFilter
-
-_suffix_to_language_id = {
-    # https://gist.github.com/ppisarczyk/43962d06686722d26d176fad46879d41
-
-    "abap": "abap",
-
-    "bat": "bat",
-
-    "cmd": "bat",
-
-    "bib": "bibtex",
-
-    "clj": "clojure",
-    "boot": "clojure",
-    "cl2": "clojure",
-    "cljc": "clojure",
-    "cljs": "clojure",
-    "cljscm": "clojure",
-    "cljx": "clojure",
-    "hic": "clojure",
-
-    "coffee": "coffeescript",
-    "_coffee": "coffeescript",
-    "cjsx": "coffeescript",
-    "cson": "coffeescript",
-    "iced": "coffeescript",
-
-    "c": "c",
-    "cats": "c",
-    "h": "c",
-    "idc": "c",
-    "w": "c",
-
-    "cpp": "cpp",
-    "cp": "cpp",
-    "c++": "cpp",
-    "cc": "cpp",
-    "cxx": "cpp",
-    "hpp": "cpp",
-    "h++": "cpp",
-    "hh": "cpp",
-    "inc": "cpp",
-    "inl": "cpp",
-    "ipp": "cpp",
-    "tcc": "cpp",
-    "tpp": "cpp",
-
-    "cs": "csharp",
-    "cake": "csharp",
-    "csx": "csharp",
-
-    "css": "css",
-
-    "dart": "dart",
-
-    "diff": "diff",
-    "patch": "diff",
-
-    "dockerfile": "dockerfile",
-
-    "ex": "elixir",
-    "exs": "elixir",
-
-    "erl": "erlang",
-    "es": "erlang",
-    "escript": "erlang",
-    "hrl": "erlang",
-    "xrl": "erlang",
-    "yrl": "erlang",
-
-    "fs": "fsharp",
-    "fsi": "fsharp",
-    "fsx": "fsharp",
-
-    "": "git-commit",
-    "": "git-rebase",
-
-    "go": "go",
-
-    "groovy": "groovy",
-    "grt": "groovy",
-    "gtpl": "groovy",
-    "gvy": "groovy",
-
-    "handlebars": "handlebars",
-    "hbs": "handlebars",
-
-    "html": "html",
-    "htm": "html",
-    "st": "html",
-    "xht": "html",
-    "xhtml": "html",
-
-    "ini": "ini",
-    "cfg": "ini",
-    "prefs": "ini",
-    "pro": "ini",
-    "properties": "ini",
-
-    "java": "java",
-
-    "js": "javascript",
-    "_js": "javascript",
-    "es6": "javascript",
-    "jsm": "javascript",
-    "njs": "javascript",
-
-    "jsx": "javascriptreact",
-
-    "json": "json",
-    "lock": "json",
-
-    "tex": "latex",
-    "ltx": "latex",
-    "aux": "latex",
-    "cbx": "latex",
-    "sty": "latex",
-    "toc": "latex",
-
-    "less": "less",
-
-    "lua": "lua",
-    "nse": "lua",
-    "pd_lua": "lua",
-    "rbxs": "lua",
-    "wlua": "lua",
-
-    "mk": "makefile",
-    "mkfile": "makefile",
-
-    "md": "markdown",
-    "markdown": "markdown",
-    "mkd": "markdown",
-    "mkdn": "markdown",
-    "mkdown": "markdown",
-
-    "m": "objective-c",
-
-    "mm": "objective-cpp",
-
-    "pl": "perl",
-    "al": "perl",
-    "cgi": "perl",
-    "ph": "perl",
-    "plx": "perl",
-    "pm": "perl",
-    "pod": "perl",
-
-    "6pl": "perl6",
-    "6pm": "perl6",
-    "nqp": "perl6",
-    "p6": "perl6",
-    "p6l": "perl6",
-    "p6m": "perl6",
-    "pl6": "perl6",
-    "pm6": "perl6",
-
-    "php": "php",
-    "aw": "php",
-    "ctp": "php",
-    "fcgi": "php",
-    "php3": "php",
-    "php4": "php",
-    "php5": "php",
-    "phps": "php",
-    "phpt": "php",
-
-    "ps1": "powershell",
-    "psm1": "powershell",
-    "psd1": "powershell",
-
-    "": "jade",
-
-    "py": "python",
-    "pyde": "python",
-    "pyp": "python",
-    "pyt": "python",
-    "pyw": "python",
-    "rpy": "python",
-    "xpy": "python",
-
-    "r": "r",
-    "rd": "r",
-    "rsx": "r",
-
-    "razor": "razor",
-    "cshtml": "razor",
-    "vbhtml": "razor",
-
-    "rb": "ruby",
-    "builder": "ruby",
-    "god": "ruby",
-    "irbrc": "ruby",
-    "rake": "ruby",
-    "rbw": "ruby",
-    "rbx": "ruby",
-    "ru": "ruby",
-    "ruby": "ruby",
-    "thor": "ruby",
-    "watchr": "ruby",
-
-    "rs": "rust",
-
-    "scss": "scss",
-
-    "sass": "sass",
-
-    "scala": "scala",
-    "sbt": "scala",
-    "sc": "scala",
-
-    "": "shaderlab",
-
-    "sh": "shellscript",
-    "bash": "shellscript",
-    "bats": "shellscript",
-    "ksh": "shellscript",
-    "zsh": "shellscript",
-    "tmux": "shellscript",
-    "command": "shellscript",
-
-    "sql": "sql",
-    "cql": "sql",
-    "ddl": "sql",
-    "prc": "sql",
-    "tab": "sql",
-    "udf": "sql",
-    "viw": "sql",
-
-    "swift": "swift",
-
-    "ts": "typescript",
-
-    "tsx": "typescriptreact",
-
-    # "tex": "tex",
-
-    "vb": "vb",
-    "bas": "vb",
-    "cls": "vb",
-    "frm": "vb",
-    "frx": "vb",
-    "vba": "vb",
-    "vbs": "vb",
-
-    "xml": "xml",
-
-    "xsl": "xsl",
-    "xslt": "xsl",
-
-    "yaml": "yaml",
-    "yml": "yaml",
-}
 
 
 @dataclass
@@ -329,6 +77,21 @@ def _expand_lsp_glob(lsp_glob: str) -> Generator[str, None, None]:
     return _expand_lsp_glob_rec(lsp_glob, selections, 0, 0)
 
 
+def _guess_language_id(path: Path) -> Optional[str]:
+    name = path.name
+
+    # Check full filename
+    if language_id := extension_to_language_id.get(name):
+        return language_id
+
+    # Check suffixes, from each . to the end
+    start = 0
+    while (start := name.find(".", start)) != -1:
+        if language_id := extension_to_language_id.get(name[start:]):
+            return language_id
+    return None
+
+
 def matches_text_document_filter(uri: str, filter: TextDocumentFilter, language_id: Optional[str] = None) -> bool:
     """
     Checks whether the given `uri` matches the `TextDocumentFilter`.
@@ -337,14 +100,13 @@ def matches_text_document_filter(uri: str, filter: TextDocumentFilter, language_
     """
 
     (scheme, _, path_raw, _, _) = urlsplit(uri, scheme="file")
-    suffix = Path(path_raw).suffix[1:]
 
     if "scheme" in filter and filter["scheme"] != scheme:
         return False
 
     if filter_language_id := filter.get("language"):
         if not language_id:
-            language_id = _suffix_to_language_id.get(suffix)
+            language_id = _guess_language_id(Path(path_raw))
         if not language_id or language_id != filter_language_id:
             return False
 
---
+++ tools/generate_languages.py
@@ -0,0 +1,169 @@
+import asyncio
+import json
+import plistlib
+from dataclasses import dataclass
+from sys import argv
+from typing import Any, Dict, List, Tuple, Union
+
+from aiohttp import ClientSession
+
+
+@dataclass(frozen=True, eq=True)
+class GitHubLocation:
+    owner: str
+    repo: str
+    path: str
+    ref: str
+
+    @classmethod
+    def from_json(cls, json: Dict[str, str]) -> "GitHubLocation":
+        return cls(json["owner"], json["repo"], json["path"], json["ref"])
+
+
+@dataclass
+class Language:
+    name: str
+    language_id: str
+    grammar_uri: Union[GitHubLocation, List[GitHubLocation], None]
+    file_extensions: List[str]
+
+
+async def generate_permalink_github(client: ClientSession, location: GitHubLocation) -> str:
+    async with client.get(f"/repos/{location.owner}/{location.repo}/git/ref/{location.ref}") as response:
+        res = await response.json()
+    commit_sha = res['object']['sha']
+    return f"https://raw.githubusercontent.com/{location.owner}/{location.repo}/{commit_sha}/{location.path}"
+
+
+async def get_grammar_scope(client: ClientSession, url: str) -> str:
+    async with client.get(url) as response:
+        if url.endswith(".json"):
+            grammar = json.loads(await response.text())
+        else:
+            grammar = plistlib.loads((await response.text()).encode(), fmt=plistlib.FMT_XML)
+    return grammar["scopeName"]
+
+
+def list_locations(languages: Dict[str, Any]) -> List[GitHubLocation]:
+    raw: List[Dict[str, Any]] = []
+    for l in languages.values():
+        raw.append(l["grammarLocation"])
+        if embedded_grammars := l.get("embeddedGrammars"):
+            raw += embedded_grammars
+
+    out: List[GitHubLocation] = []
+    for l in raw:
+        if not l["type"] == "github":
+            raise ValueError("Location type not supported: " + l["type"])
+        out.append(GitHubLocation.from_json(l))
+
+    return out
+
+
+async def locations_to_urls(locations: List[GitHubLocation], token: str) -> List[str]:
+    async with ClientSession("https://api.github.com/", headers={"Authorization": "Bearer " + token}) as client:
+        requests = [generate_permalink_github(client, location) for location in locations]
+        return await asyncio.gather(*requests)
+
+
+async def urls_to_scopes(urls: List[str], token: str) -> List[str]:
+    async with ClientSession(headers={"Authorization": "Bearer " + token}) as client:
+        requests = [get_grammar_scope(client, url) for url in urls]
+        return await asyncio.gather(*requests)
+
+
+async def generate_scope_maps(languages: Dict[str, Any], token: str) -> Tuple[Dict[str, str], Dict[str, str]]:
+    locations = list_locations(languages)
+    urls = await locations_to_urls(locations, token)
+    scopes = await urls_to_scopes(urls, token)
+
+    location_to_scope = dict(zip(locations, scopes))
+
+    language_id_to_scope: Dict[str, str] = {}
+    for id, lang in languages.items():
+        location = GitHubLocation.from_json(lang["grammarLocation"])
+        language_id_to_scope[id] = location_to_scope[location]
+
+    scope_to_grammar = dict(zip(scopes, urls))
+
+    return (language_id_to_scope, scope_to_grammar)
+
+
+language_priorities: Dict[str, int] = {
+    "c": 100,
+    "cpp": 200,
+    "coffeescript": 200,
+    "csharp": 100,
+    "fsharp": 200,
+    "glsl": 100,
+    "razor": 200,
+    "sdl": 100,
+    "sml": 200,
+    "genie": 200,
+    "glsl": 100
+}
+
+
+def update_extension_map(mapping: Dict[str, str], language_id: str, extension: str) -> None:
+    current_id = mapping.get(extension)
+    if current_id:
+        priority_current = language_priorities.get(current_id)
+        priority_new = language_priorities.get(language_id)
+        if priority_current is None or priority_new is None:
+            raise ValueError(
+                f"Priorities for language ids {current_id} and {language_id} required, because of extension '{extension}'")
+        if priority_current >= priority_new:
+            return
+    mapping[extension] = language_id
+
+
+def generate_extension_map(languages: Dict[str, Any]) -> Dict[str, str]:
+    mapping: Dict[str, str] = {}
+    for id, val in languages.items():
+        for e in val["extensions"]:
+            update_extension_map(mapping, id, e)
+    return mapping
+
+
+def generate_languages_py(extension_to_language_id: Dict[str, str], language_id_to_scope: Dict[str, str], scope_to_grammar: Dict[str, str]) -> str:
+    extension_to_language_id_str = "\n".join(f'    "{key}": "{val}",' for key, val in extension_to_language_id.items())
+    language_id_to_scope_str = "\n".join(f'    "{key}": "{val}",' for key, val in language_id_to_scope.items())
+    scope_to_grammar_str = "\n".join(f'    "{key}": "{val}",' for key, val in scope_to_grammar.items())
+
+    return f"""\
+extension_to_language_id = {{
+{extension_to_language_id_str}
+}}
+
+
+language_id_to_scope = {{
+{language_id_to_scope_str}
+}}
+
+
+scope_to_grammar = {{
+{scope_to_grammar_str}
+}}
+"""
+
+
+async def main() -> None:
+    with open("languages.json") as file:
+        languages = json.load(file)
+
+    with open("github-token") as file:
+        token = file.read().strip()
+
+    language_id_to_scope, scope_to_grammar = await generate_scope_maps(languages, token)
+
+    extension_to_language_id = generate_extension_map(languages)
+
+    out_str = generate_languages_py(extension_to_language_id, language_id_to_scope, scope_to_grammar)
+    with open(argv[1], "w") as file:
+        file.write(out_str)
+
+    await asyncio.sleep(0.3)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

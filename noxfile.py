334993c4299818f3060c4025743ec56579c047d6
Jonas Rinke <delphi1024@gmail.com>
2023-11-18 21:15:20 +0100

    Added nox devenv session

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,10 @@
-from functools import wraps
-from typing import Callable, Dict
+from pathlib import Path
 
 import nox
+
+MIN_PYTHON = "3.8"
+SUPPORTED_PYTHONS = ["3.8", "3.9", "3.10"]
+
 
 nox.options.sessions = ["init", "test", "quality"]
 nox.options.pythons = ["3.8"]
@@ -12,28 +15,23 @@ DEP_TEST = ["pytest-asyncio", *DEP_PYTEST]
 DEP_PYLINT = ["pylint~=3.0"]
 DEP_BLACK = ["black~=23.11"]
 DEP_LINT = [*DEP_PYLINT, *DEP_PYTEST]
-
-
-_called_sessions: Dict[str, bool] = {}
-
-
-def once(func: Callable[[nox.Session], None]) -> Callable[[nox.Session], None]:
-    @wraps(func)
-    def wrapper(session: nox.Session) -> None:
-        if not _called_sessions.get(session.name):
-            func(session)
-            _called_sessions[session.name] = True
-
-    return wrapper
+DEP_ALL = [*DEP_TEST, *DEP_PYLINT, *DEP_BLACK]
 
 
 _initialized = False
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def init(session: nox.Session) -> None:
     """
     Initialize the repository for development and testing.
+
+    Some sessions that require the repository to be initialized will call this
+    session. Even if multiple sessions call ``init``, it will run at most once
+    per nox invokation.
+
+    When the command-line argument ``--initialized`` is passed to nox, ``init``
+    assumes that the repository is already in an initialized state and does nothing.
     """
     global _initialized
     if _initialized or "--initialized" in session.posargs:
@@ -44,7 +42,7 @@ def init(session: nox.Session) -> None:
     _initialized = True
 
 
-@nox.session(python=["3.8", "3.9", "3.10"])
+@nox.session(python=SUPPORTED_PYTHONS)
 def test(session: nox.Session) -> None:
     """
     Run unit tests for change-ls
@@ -55,7 +53,7 @@ def test(session: nox.Session) -> None:
     session.run("pytest", "-m", "not uses_external_resources", "test")
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def quality(session: nox.Session) -> None:
     """
     Run various code quality checks (type checking, linting).
@@ -103,7 +101,7 @@ def build_token_server(session: nox.Session) -> None:
         session.run("npm", "run", "build", external=True)
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def test_generator(session: nox.Session) -> None:
     """
     Runs the tests for the change-ls code generator.
@@ -112,7 +110,7 @@ def test_generator(session: nox.Session) -> None:
     session.run("pytest", "gen/test")
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def generate_lsp_types(session: nox.Session) -> None:
     """
     Generates the code for the LSP types at ``change_ls/types``.
@@ -121,7 +119,7 @@ def generate_lsp_types(session: nox.Session) -> None:
     session.run("python", "-m", "gen.main", "./res", "./change_ls/types")
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def reformat(session: nox.Session) -> None:
     """
     Reformat the code using Black.
@@ -131,7 +129,7 @@ def reformat(session: nox.Session) -> None:
     session.run("black", "change_ls", "gen", "test")
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def check_formatting(session: nox.Session) -> None:
     """
     Check whether the code adheres to the configured style using Black.
@@ -141,7 +139,7 @@ def check_formatting(session: nox.Session) -> None:
     session.run("black", "--check", "change_ls", "gen", "test")
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def lint(session: nox.Session) -> None:
     """
     Perform linting using pylint.
@@ -151,7 +149,7 @@ def lint(session: nox.Session) -> None:
     session.run("pylint", "--disable=R,C", "change_ls", "gen")
 
 
-@nox.session(python=["3.8"])
+@nox.session(python=MIN_PYTHON)
 def typecheck(session: nox.Session) -> None:
     """
     Perform type checking using pyright.
@@ -161,3 +159,42 @@ def typecheck(session: nox.Session) -> None:
     session.run(
         "npm", "exec", "--package", "pyright@1.1.335", "--yes", "--", "pyright", external=True
     )
+
+
+def _get_python_in_virtualenv(session: nox.Session) -> str:
+    devenv_name = ".venv"
+    if "--name" in session.posargs:
+        name_index = session.posargs.index("--name")
+        if name_index + 1 < len(session.posargs):
+            devenv_name = session.posargs[name_index + 1]
+
+    devenv_location = Path("./" + devenv_name)
+    session.install("virtualenv")
+    session.run("virtualenv", str(devenv_location), "--python", "cpython" + MIN_PYTHON)
+
+    devenv_python = devenv_location / Path("bin/python")
+    if not devenv_python.exists():
+        devenv_python = devenv_location / Path("Scripts/python.exe")
+
+    return str(devenv_python)
+
+
+@nox.session(python=MIN_PYTHON)
+def devenv(session: nox.Session) -> None:
+    """
+    Set up a virtual development environemnt.
+
+    Use ``--global`` to install into the global environment.
+
+    Use ``--name <name>`` to set the name for the development environment.
+    The default name is ``.venv``.
+    """
+    init(session)
+
+    if "--global" in session.posargs:
+        python = "python"
+    else:
+        python = _get_python_in_virtualenv(session)
+
+    session.run(python, "-m", "pip", "install", "-e", ".", external=True)
+    session.run(python, "-m", "pip", "install", *DEP_ALL, external=True)
